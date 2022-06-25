5c6e8d146a56ecdd58334b5fa3aa93dff69628b7
Matheus Sameck <105814501+sameckmatheus@users.noreply.github.com>
2022-06-25 09:28:09 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 2626;
 app.listen(PORT, () => {
   console.log(`App listenner on port ${PORT}`);
 })
