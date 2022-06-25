6267da402954f0d82ddba700ef32380b25fe89b9
Matheus Sameck <105814501+sameckmatheus@users.noreply.github.com>
2022-06-25 09:28:54 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 const { Router } = require('express');
-
 const UserController = require('./app/Controllers/UserController');
-
 const routes = new Router();
 
-routes.post("/user", UserController.store);
 routes.get("/user", UserController.show);
+routes.post("/user", UserController.store);
 
 module.exports = routes;
